35dc96e1748f694036eac3e1d3d6e9c085bce178
GABRIELA MARIA ROCHA BOLAINA <gabibolaina@gmail.com>
2022-03-25 14:37:34 -0300

    Organizacao
    Divisão entre css, javascript e html

---
+++ _javascript/funcoes.js
@@ -0,0 +1,6 @@
+//window.alert('Seja bem vind@! Lembre-se que este é um algoritmo muito complexo, números altos demoraram a ser computados e poderão causar travamentos do sistema.')
+
+function verificarNum() {
+    resultadoFinal = "É primo"
+    document.getElementById("cResultado").innerHTML = resultadoFinal 
+    }
